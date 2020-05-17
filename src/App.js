9706cf7df26a07a3ce8473a538b8635c61349719
gonjavi <javiercaliescali@gmail.com>
2020-05-16 22:39:10 -0500

    create articulo informativo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import ArticuloPublicitario from './components/ArticuloPublicitario';
+import ArticuloTecnologia from './components/ArticuloTecnologia';
+
 function App() {
   return (
     <div className="App">
      <NavBar />
      <ArticuloPublicitario />
+     <ArticuloTecnologia />
     </div>
   );
 }
---
+++ src/components/ArticuloTecnologia.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class ArticuloTecnologia extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h3 className="center">Articulo de tecnología:</h3>
+        <div className="row">
+          <h5 className="col l7 offset-l3">Dentro de los frameworks mas importantes de la actualidad de desarrollo web back-end podemos mencianar ruby on rails
+          Djngo, Laravel, para front-end las tecnologias mas utilizadas ahora mismo son ReactJS, Angular, Vue, Bootstrap y Materialize-CSS. </h5>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ArticuloTecnologia;
