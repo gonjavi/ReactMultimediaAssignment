507ecb3c1485823aa3428a61d2d78d7d29c548d8
gonjavi <javiercaliescali@gmail.com>
2020-05-17 00:02:53 -0500

    add panel hoverable

--- src/components/ArticuloPublicitario.js
+++ src/components/ArticuloPublicitario.js
@@ -7,8 +7,9 @@ class ArticuloPublicitario extends Component {
         
         <h3 className="center">Incripciones Abiertas:</h3>
         <div className="row">
-          <h5 className="col l9 offset-l2">Las incripciones se encuentran abiertas a nivel nacional, si deseas información llama el numero: 01800 234875 </h5>
+          <h5 className="col l8 offset-l2 card-panel hoverable">Las incripciones se encuentran abiertas a nivel nacional, si deseas información llama el numero: 01800 234875 </h5>
         </div>
+        
       </div>
     )
   }
--- src/components/ArticuloTecnologia.js
+++ src/components/ArticuloTecnologia.js
@@ -4,9 +4,9 @@ class ArticuloTecnologia extends Component {
   render() {
     return (
       <div className="container">
-        <h3 className="center">Articulo de tecnología:</h3>
+        <h3 className="center">Articulo de Tecnología:</h3>
         <div className="row">
-          <h5 className="col l9 offset-l2">Dentro de los frameworks mas importantes de la actualidad de desarrollo web back-end podemos mencionar Ruby on Rails
+          <h5 className="col l8 offset-l2 card-panel hoverable">Dentro de los frameworks mas importantes de la actualidad de desarrollo web back-end podemos mencionar Ruby on Rails
           Djngo, Laravel. Para front-end las tecnologias mas utilizadas ahora mismo son ReactJS, Angular, Vue, Bootstrap y Materialize-CSS. </h5>
         </div>
       </div>
